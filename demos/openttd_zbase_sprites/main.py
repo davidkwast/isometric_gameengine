ab055bab3ec9ea95fcc60475657f2c13631ba5ee
David Kwast <david@kwast.me>
2019-11-03 23:06:20 -0300

    EngineObject with auto increment NEXT_ID, moving unit test

--- demos/openttd_zbase_sprites/main.py
+++ demos/openttd_zbase_sprites/main.py
@@ -1,11 +1,71 @@
+from enum import Enum
+
 import pyglet
+
+
+class EngineObject:
+    
+    NEXT_ID = 1
+
+    def __init__(self):
+        self.unit_id = NEXT_ID
+        NEXT_ID += 1
+
+# class GraphicUnit(EngineObject):
+#     def __init__(self, pyglet_batch: pyglet.graphics.Batch):
+#         self.pyglet_batch = pyglet_batch
+
+# class TerrainTile(GraphicUnit):
+#     def __init__(self, )
+
+
+class Direction(Enum):
+    NORTH = ( 0, -1)
+    EAST =  ( 1,  0)
+    SOUTH = ( 0,  1)
+    WEST  = (-1,  0)
+
+# class DirectionalGraphicUnit:
+#     def __init__(self, directional_sprite_dict):
+#         self.directional_sprite_dict = directional_sprite_dict
+
+class CoalTruck(EngineObject):
+    def __init__(self, batch):
+        self.position = 0*128, 720-2*64
+        self.go_to = 6*128, 720-8*64
+        self.direction = Direction.EAST
+        self.moving_flag = False
+        self.moving_direction = Direction.EAST
+        self.image = pyglet.resource.image('assets/truck_coal/256_0563.png')
+        self.sprite = pyglet.sprite.Sprite(img=self.image, batch=batch)
+        self.sprite.x, self.sprite.y = self.position
+    
+    def update(self, dt):
+        pos = self.position
+        gt = self.go_to
+        
+        if pos == gt: return False
+
+        s = self.sprite
+        s.x += int(128 * dt)
+        s.y += int(-64 * dt)
+        
+        if s.x > gt[0]:
+            s.x = gt[0]
+        if s.y < gt[1]:
+            s.y = gt[1]
+        
+        self.position = s.x, s.y
+
+        return True
+
+
 
 window = pyglet.window.Window(resizable=True)
 #window.maximize()
 window.set_location(0, 0)
 window.set_size(1280, 720)
 
-image_bus = pyglet.resource.image('assets/truck_coal/256_0563.png')
 image_grass = pyglet.resource.image('assets/land_grass/256_0001.png')
 
 batch = pyglet.graphics.Batch()
@@ -13,25 +73,26 @@ batch = pyglet.graphics.Batch()
 sprite_grass = pyglet.sprite.Sprite(img=image_grass, batch=batch)
 sprite_grass.x, sprite_grass.y = 0, 720-128
 
-sprite_bus_01 = pyglet.sprite.Sprite(img=image_bus, batch=batch)
-sprite_bus_01.y = 720-128
-sprite_bus_01.fx, sprite_bus_01.fy = float(sprite_bus_01.x), float(sprite_bus_01.y)
 
-# fps_display = pyglet.window.FPSDisplay(window)
+moving_units = [
+    CoalTruck(batch),
+]
+
+
+
+fps_display = pyglet.window.FPSDisplay(window)
+
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-    # fps_display.draw()
+    fps_display.draw()
 
 def update(dt):
-    s = sprite_bus_01
-    s.fx = (s.fx + dt*60) % 720
-    s.fy = s.fy - dt*30
-    if s.fy < -128:
-         s.fy = 720-128
-    s.x, s.y = int(s.fx), int(s.fy)
 
-# pyglet.clock.set_fps_limit(60)
-pyglet.clock.schedule_interval(update, 0.015)
+    for unit in moving_units:
+        unit.update(dt)
+
+
+pyglet.clock.schedule_interval(update, 1/60)
 pyglet.app.run()
